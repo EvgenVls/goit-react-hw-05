868c12ab25286b49366f9fd2195548313628b232
Yevgen Vlasenko <evgenvl@ukr.net>
2024-05-07 21:39:29 +0300

    form styles

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,6 +4,9 @@ import css from "./MovieList.module.css";
 export default function MovieList({ movies }) {
   const location = useLocation();
 
+  const defaultImg =
+    "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
+
   return (
     <ul className={css.list}>
       {movies.map((movie) => (
@@ -14,7 +17,11 @@ export default function MovieList({ movies }) {
             className={css.link}
           >
             <img
-              src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+              src={
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+                  : defaultImg
+              }
               alt={movie.title}
             />
             <p className={css.movieTitle}>{movie.title}</p>
